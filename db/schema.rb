46baf9520df4f5352208c3d43b0e54e6649f51c3
erika paez <eppaez59@ucatolica.edu.co>
2018-10-15 19:31:56 -0500

    tabla area
    cree tabla area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_234847) do
+ActiveRecord::Schema.define(version: 2018_10_16_001550) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "nameArea", limit: 20
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "location", limit: 20
